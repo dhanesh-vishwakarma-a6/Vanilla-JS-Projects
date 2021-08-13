996066b529f44d50c78a8d130b76889288823bbc
Dhanesh Vishwakarma <dhanesh.vishwakarma11@gmail.com>
2021-08-13 22:03:21 +0530

    updates needed

---
+++ index.js
@@ -0,0 +1,8 @@
+//
+// const phoneScreenSize = 375
+// if (window.screen != null && window.screen.width <= phoneScreenSize) {
+//   const xWidth = window.screen.width
+// }
+
+// document.getElementById("btn-sidebar")
+//   .addEventListener("click", () => console.log("clicked"))
