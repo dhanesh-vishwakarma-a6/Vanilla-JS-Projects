aca5c3c7884fe43ee815880ae1b92cb4ae1b6e57
Dhanesh Vishwakarma <dhanesh.vishwakarma11@gmail.com>
2021-08-20 11:46:34 +0530

    added active & inactive tab & it's article feature

---
+++ 11-tabs/app.js
@@ -0,0 +1,25 @@
+const btns = document.querySelectorAll(".tab-btn")
+const about = document.querySelector(".about")
+const articles = document.querySelectorAll(".content")
+
+about.addEventListener("click", function (event) {
+    const id = event.target.dataset.id
+    if (id) {
+        // tabs
+        btns.forEach(function (btn) {
+            //  inactive all other tabs
+            btn.classList.remove("active")
+            // active the current tab
+            event.target.classList.add("active")
+        })
+
+        // articles
+        articles.forEach(function (article) {
+            // inactive all other articles
+            article.classList.remove("active")
+            // active the current article
+            const element = document.getElementById(id)
+            element.classList.add("active")
+        })
+    }
+})
